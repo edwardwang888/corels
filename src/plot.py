25e0171768d60cd4d9b3c903aba65197693628f5
Edward Wang <edwardwang@Edwards-MacBook-Pro-3.local>
2018-08-14 15:50:13 -0700

    Also plot baseline results

--- src/plot.py
+++ src/plot.py
@@ -3,13 +3,20 @@ sys.path[:0] = ["/Library/Python/2.7/site-packages/"]
 import numpy as np
 import matplotlib.pyplot as plt
 
-dt = np.dtype([('Regularization',np.float32),('Length',np.int32),('Accuracy',np.float32),('Objective',np.float32)])
+dt1 = np.dtype([('Regularization',np.float32),('Length',np.int32),('Accuracy',np.float32),('Objective',np.float32)])
+dt2 = np.dtype([('Regularization',np.float32),('Objective',np.float32)])
 data = []
 names = []
+plot_all = True
 for i in range(1, len(sys.argv)):
-    data.append(np.genfromtxt(sys.argv[i], delimiter=',', skip_header=1, dtype=dt))
+    filename = sys.argv[i]
+    if "logistic" in filename or "rforest" in filename:
+        plot_all = False
+        data.append(np.genfromtxt(filename, delimiter=' ', dtype=dt2))
+    else:
+        data.append(np.genfromtxt(filename, delimiter=',', skip_header=1, dtype=dt1))
     # Remove csv extension
-    names.append(sys.argv[i][:-4])
+    names.append(filename[:-4])
 
 if len(data) <= 2:
     marker = ['x','+']
@@ -30,28 +37,33 @@ plt.ylabel('Min Objective')
 plt.title("Regularization vs. Min Objective")
 plt.legend()
 
-for i in data:
-    i.sort(order=['Length','Accuracy'])
 
 
+if plot_all:
+    for i in data:
+        i.sort(order=['Length','Accuracy'])
 
 #Plot list length vs. accuracy
-plt.figure(2)
-for i in range(len(data)):
-    plt.plot(data[i][:]['Length'], data[i][:]['Accuracy'], marker[i] + '--', label=names[i])
-plt.title("Accuracy vs. Length of Rule List")
-plt.xlabel('Length of Rule List')
-plt.ylabel('Accuracy')
-plt.legend()
+if plot_all:
+    plt.figure(2)
+    for i in range(len(data)):
+        plt.plot(data[i][:]['Length'], data[i][:]['Accuracy'], marker[i] + '--', label=names[i])
+    plt.title("Accuracy vs. Length of Rule List")
+    plt.xlabel('Length of Rule List')
+    plt.ylabel('Accuracy')
+    plt.legend()
 
 
 
 #Plot list length vs. objective
-plt.figure(3)
-for i in range(len(data)):
-    plt.plot(data[i][:]['Length'], data[i][:]['Objective'], marker[i], label=names[i])
-plt.title("Min Objective vs. Length of Rule List")
-plt.xlabel('Length of Rule List')
-plt.ylabel('Min Objective')
-plt.legend()
+if plot_all:
+    plt.figure(3)
+    for i in range(len(data)):
+        plt.plot(data[i][:]['Length'], data[i][:]['Objective'], marker[i], label=names[i])
+    plt.title("Min Objective vs. Length of Rule List")
+    plt.xlabel('Length of Rule List')
+    plt.ylabel('Min Objective')
+    plt.legend()
+    
+    
 plt.show()
