b8eb0df4b176c378296168cb22a516ad3fb2316b
Edward Wang <edwardwang@Edwards-MacBook-Pro-3.local>
2018-08-22 11:47:37 -0700

    Only plot ROC with max AUC

--- src/roc.py
+++ src/roc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import auc
 
 i = 1
 
@@ -13,21 +14,35 @@ def write_to_file(fpr, tpr, outfile_roc):
         f.write("\n")
         f.close()
 
-def plot(outfile_roc):
-    global i
-    plt.figure(i)
+def find_max_roc(outfile_roc):
     with open(outfile_roc, 'rb') as f:
         data = f.readlines()
     
+    max_auc = 0
+    max_i = 0
     for i in range(len(data)/2):
         fpr = np.genfromtxt([data[2*i].rstrip()])
         tpr = np.genfromtxt([data[2*i+1].rstrip()])
-        plt.plot(fpr, tpr)
+        roc_auc = auc(fpr, tpr)
+        if roc_auc > max_auc:
+            max_auc = roc_auc
+            max_i = i
+
+    fpr = np.genfromtxt([data[2*max_i].rstrip()])
+    tpr = np.genfromtxt([data[2*max_i+1].rstrip()])
+    return fpr, tpr
+
+def plot(outfile_roc):
+    global i
+    plt.figure(i)
+    fpr, tpr = find_max_roc(outfile_roc)
+    max_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr)
 
     plt.title("Receiver Operating Characteristic")
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.plot([0], [0], label=outfile_roc.replace(".csv", ""))
+    plt.plot([0], [0], label=outfile_roc.replace(".csv", "") + "\n(AUC = {})".format(max_auc))
     plt.legend()
     i += 1
 
